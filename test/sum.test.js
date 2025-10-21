2dbc14ab7b3a97159bcfa04d8610f5e1a7a6e165
laszlokeresztescv <laszlo.keresztes.cv@gmail.com>
2025-10-21 08:38:56 +0300

    add workflow- hozzadatam a z eljarast

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,8 +1,7 @@
 import {describe, it, expect} from 'vitest';
 import { sum } from '../src/sum.js';
 describe('sum function', () => {
-    it('add ossze a szamokat, () => {
-        expect(sum(2, 3)).toBe(5); 
+    it('add ossze a szamokat',()=>{expect(sum(2, 3)).toBe(5); 
     });
 });
 
